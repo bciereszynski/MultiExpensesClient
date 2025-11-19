074912a81049287efe08bbdf9d938a0236ce4e01
bciereszynski <ciereszynskibp@gmail.com>
2025-11-19 15:43:29 +0100

    ME-1: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Signup } from './components/signup/signup';
+import { Login } from './components/login/login';
+import { TransactionForm } from './components/transaction-form/transaction-form';
+import { TransactionList } from './components/transaction-list/transaction-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: Login
+    },
+    {
+        path:'signup',
+        component: Signup
+    },
+    {
+        path: 'transactions',
+        component: TransactionList
+    },
+    {
+        path:'add',
+        component: TransactionForm
+    },
+    {
+        path:'edit/:id',
+        component: TransactionForm
+    },
+    {
+        path:'',
+        redirectTo:'/transactions',
+        pathMatch:'full'
+    },
+    {
+        path:'**',
+        redirectTo:'/transactions'
+    }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { TransactionList } from './components/transaction-list/transaction-list';
+import { TransactionForm } from './components/transaction-form/transaction-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, TransactionList, TransactionForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
