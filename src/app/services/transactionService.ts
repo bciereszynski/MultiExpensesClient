078c9b9be548845f97159c13374ef1b15f5842f0
bciereszynski <ciereszynskibp@gmail.com>
2025-11-19 15:43:49 +0100

    ME-1: add transaction service

---
+++ src/app/services/transactionService.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transaction } from '../models/transaction';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  private apiUrl = 'https://localhost:7163/api/Transactions'
+
+  constructor(private http: HttpClient){
+
+  }
+
+  getAll(): Observable<Transaction[]>{
+    return this.http.get<Transaction[]>(this.apiUrl+"/All");
+  }
+
+  getById(id: number): Observable<Transaction>{
+    return this.http.get<Transaction>(this.apiUrl+"/Details/"+id);
+  }
+
+  create(transaction:Transaction): Observable<Transaction>{
+    return this.http.post<Transaction>(this.apiUrl+"/Create/", transaction);
+  }
+
+  update(transaction:Transaction): Observable<Transaction>{
+    return this.http.post<Transaction>(this.apiUrl+"/Update/", transaction);
+  }
+
+  delete(id: number): Observable<void>{
+    return this.http.delete<void>(this.apiUrl+"/Delete/"+id);
+  }
+}
