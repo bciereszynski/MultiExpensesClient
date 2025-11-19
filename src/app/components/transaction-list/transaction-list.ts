08950288bef0eb1131a391ed18023e7b1e0c70b8
bciereszynski <ciereszynskibp@gmail.com>
2025-11-19 17:40:53 +0100

    ME-1: add transaction list

--- src/app/components/transaction-list/transaction-list.ts
+++ src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from '../../models/transaction';
+import { CommonModule } from '@angular/common';
+import { TransactionService } from '../../services/transactionService';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-list.html',
   styleUrl: './transaction-list.css',
 })
-export class TransactionList {
+export class TransactionList implements OnInit {
+  transactions: Transaction[] = [];
+  constructor(private transactionService: TransactionService) {}
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
 
+  loadTransactions(): void {
+    this.transactionService
+    .getAll()
+    .subscribe((data) => {
+      this.transactions = data;
+    });
+  }
 }
