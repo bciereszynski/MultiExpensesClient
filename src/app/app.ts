f882d8869ee61bfb1ebec61189520614f30b045a
bciereszynski <ciereszynskibp@gmail.com>
2025-11-20 01:06:02 +0100

    ME-1: remove warnings

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { TransactionList } from './components/transaction-list/transaction-list';
-import { TransactionForm } from './components/transaction-form/transaction-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, TransactionList, TransactionForm],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
