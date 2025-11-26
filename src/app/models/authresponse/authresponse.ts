a9ade1e8b672771df525d101e5f90b8cfd448c8d
bciereszynski <ciereszynskibp@gmail.com>
2025-11-26 23:14:00 +0100

    ME-2: add auth model and services

---
+++ src/app/models/authresponse/authresponse.ts
@@ -0,0 +1,4 @@
+export interface Authresponse {
+  token: string;
+  email: string;
+}
---
+++ src/app/services/authService.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user/user';
+import { Observable, tap } from 'rxjs';
+import { Authresponse } from '../models/authresponse/authresponse';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'https://localhost:7163/api/Auth'
+
+  constructor(private http: HttpClient, private router: Router) {
+
+  }
+
+  login(credentials: User): Observable<Authresponse> {
+    return this.http.post<Authresponse>(this.apiUrl + '/login', credentials).pipe(tap(response => {
+      localStorage.setItem('authToken', response.token);
+    }));
+  }
+
+  register(credentials: User): Observable<Authresponse> {
+    return this.http.post<Authresponse>(this.apiUrl + '/register', credentials).pipe(tap(response => {
+      localStorage.setItem('authToken', response.token);
+    }));
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    this.router.navigate(['/login']);
+  }
+
+}
